6ea10de321caa7dd9882e472af220136772a9c64
ademdogu <doguadem@gmail.com>
2020-08-05 14:57:41 -0700

    Initial commit

---
+++ src/main/java/asd/springframework/asdbeerservice/AsdBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package asd.springframework.asdbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsdBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsdBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/asd/springframework/asdbeerservice/AsdBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package asd.springframework.asdbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsdBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
