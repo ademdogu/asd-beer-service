18dc4a0b4c064204011c3bab771f53e6223898b1
ademdogu <doguadem@gmail.com>
2020-08-06 15:13:44 -0700

    Creating JPA entity for Beer object. closes #4

--- src/main/java/asd/springframework/asdbeerservice/web/controller/BeerController.java
+++ src/main/java/asd/springframework/asdbeerservice/web/controller/BeerController.java
@@ -1,12 +1,14 @@
 package asd.springframework.asdbeerservice.web.controller;
 
 import asd.springframework.asdbeerservice.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@Slf4j
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
@@ -21,6 +23,7 @@ public class BeerController {
     @PostMapping
     public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
 
+        log.debug(beerDto.toString());
         // todo impl
         return new ResponseEntity(HttpStatus.CREATED);
     }
